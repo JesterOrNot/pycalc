47e64f3acdcf337e7f482607342bd67b8ba05525
JesterOrNot <seanhellum45@gmail.com>
2019-07-08 03:45:56 +0000

    new file:   lf.py

---
+++ Physics Calc/lf.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python3.7
+# This is a lorentz factor calc
+import math
+
+def lf():
+    v = float(input('What is v?: '))
+    c = float(input("What is c?: "))
+    equ = 1 / math.sqrt(1-(v ** 2)/(c**2))
+    print(equ)
+lf()
