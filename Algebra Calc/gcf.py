177b5e088c5302a84d8ac3329652803cea5e7ac3
Sean Hellum <seanhellum45@gmail.com>
2019-07-09 13:40:56 -0500

    modified:   Algebra Calc/gcf.py

--- Algebra Calc/gcf.py
+++ Algebra Calc/gcf.py
@@ -1,19 +1,8 @@
 #! /usr/bin/python3.7
-import math
-from sympy import *
-dict1 = {
-    
-}
-def gcf():
-    num1 = int(input('What is the first number?: '))
-    num2 = int(input('What is the second number?: '))
-    gcf = math.gcd(num1, num2)
-    dict1.update({"gcf":gcf})
-    print('The GCF of {} and {} is {}.'.format(num1, num2, gcf))
-while True:
-    try:
-        gcf()
-        break
-    except ValueError:
-        print("You can't use a decimal, please try again")
-        gcf()
+import sympy
+def test2():
+    usrInt = input('What numbers are we getting the gcf of?: ')
+    my_list = usrInt.replace(" "," ").split(",")
+    z = sympy.gcd_list(my_list)
+    print(z)
+test2()
