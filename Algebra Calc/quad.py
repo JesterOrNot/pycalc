2b7102889706aaea9429140f0595b94421928993
Sean Hellum <seanhellum45@gmail.com>
2019-07-09 19:55:19 -0500

    Update quad.py

--- Algebra Calc/quad.py
+++ Algebra Calc/quad.py
@@ -2,9 +2,14 @@
 
 
 from math import sqrt
-a = float(input('What is a?: '))
-b = float(input('What is b?: '))
-c = float(input('What is c?: '))
-xplus = ( (b * -1) + (sqrt( (b ** 2) - (4* a * c) )) ) / (2 * a)
-xminus = ( (b * -1) - (sqrt( (b ** 2) - (4* a * c) )) ) / (2 * a)
-print('x = {} or x = {}'.format(xplus, xminus))
+def quad():
+    a = float(input('What is a?: '))
+    b = float(input('What is b?: '))
+    c = float(input('What is c?: '))
+    xplus = ( (b * -1) + (sqrt( (b ** 2) - (4* a * c) )) ) / (2 * a)
+    xminus = ( (b * -1) - (sqrt( (b ** 2) - (4* a * c) )) ) / (2 * a)
+    print('x = {} or x = {}'.format(xplus, xminus))
+try:
+    quad()
+except ValueError():
+    print('Error: Tip, you cant take the square root of a negative number see if that is the issue!')
