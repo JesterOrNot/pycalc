224900355fea78a3a5592704adabdfc7212ac64c
Sean Hellum <seanhellum45@gmail.com>
2019-07-15 12:01:13 -0500

    modified:   Algebra Calc/main.py

--- Algebra Calc/main.py
+++ Algebra Calc/main.py
@@ -4,27 +4,26 @@ while True:
     def calc():
         userInt = input('What calculator do you want?: ').lower()
         if userInt in 'les':
-          import les
+            import les
         elif userInt in 'quad, quadratic':
-          import quad
+            import quad
         elif userInt in 'av':
-          import av
+            import av
         elif userInt in 'factor':
-          import f
+            import f
         elif userInt in 'zpp':
-          import zpp
+            import zpp
         elif userInt in 'gcf':
-          import gcf
+            import gcf
         elif userInt in 'los':
-          import los
+            import los
         elif userInt in 'sfqg':
-          import sfqg
+            import sfqg
         elif userInt == 'exit':
-          exit()
+            exit()
         else:
-          print('Error something went wrong. Please try again')
+            print('Error something went wrong. Please try again')
     calc()
-
 
 
 # This is the  controller file for the algebra calculator this is where the user input is read and is used to access the diffrent functions of the calculator
