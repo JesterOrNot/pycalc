067728defa0ec02cf04242b2ba1b7f7e20059dc9
Sean Hellum <seanhellum45@gmail.com>
2019-07-16 07:56:35 -0500

    modified:   Algebra Calc/los.py 	modified:   Algebra Calc/sfqg.py

--- Algebra Calc/los.py
+++ Algebra Calc/los.py
@@ -3,6 +3,8 @@ def lineOfSym():
     print('Welcome this is the line of symetrey calc')
     b = float(input('What is b?: '))
     a = float(input('What is a?: '))
-    los = b*-1/2*a
+    los = (b*-1) / (2*a)
     print('The line of symemetey is x = {}'.format(los))
+
+
 lineOfSym()
--- Algebra Calc/sfqg.py
+++ Algebra Calc/sfqg.py
@@ -1,28 +1,36 @@
 from math import sqrt
 import sympy
-dict1 ={
-	
+dict1 = {
+
 }
+
+
 def get_Value():
-	a = float(input("What is a?: "))
-	dict1.update({"a" : a})
-	b = float(input("What is b?: "))
-	dict1.update({"b" : b})
-	c = float(input("What is c?: "))
-	dict1.update({"c" : c})
+    a = float(input("What is a?: "))
+    dict1.update({"a": a})
+    b = float(input("What is b?: "))
+    dict1.update({"b": b})
+    c = float(input("What is c?: "))
+    dict1.update({"c": c})
+
+
 def lineOfSym():
-	a = dict1.get("a")
-	b = dict1.get("b")
-	los = b*-1/2*a
-	dict1.update({"los" : los})
-	print('The line of symemetey is x = {}'.format(los))
+    a = dict1.get("a")
+    b = dict1.get("b")
+    los = (b*-1) / (2*a)
+    dict1.update({"los": los})
+    print('The line of symemetey is x = {}'.format(los))
+
+
 def quad():
-	a = dict1.get("a")
-	b = dict1.get("b")
-	c = dict1.get("c")
-	xplus = ( (b * -1) + (sqrt( (b ** 2) - (4* a * c) )) ) / (2 * a)
-	xminus = ( (b * -1) - (sqrt( (b ** 2) - (4* a * c) )) ) / (2 * a)
-	print('The x intercepts are {} and {}'.format(xplus, xminus))
+    a = dict1.get("a")
+    b = dict1.get("b")
+    c = dict1.get("c")
+    xplus = ((b * -1) + (sqrt((b ** 2) - (4 * a * c)))) / (2 * a)
+    xminus = ((b * -1) - (sqrt((b ** 2) - (4 * a * c)))) / (2 * a)
+    print('The x intercepts are {} and {}'.format(xplus, xminus))
+
+
 def vertex():
     a = dict1.get("a")
     b = dict1.get("b")
@@ -33,10 +41,12 @@ def vertex():
     c1 = c
     y = a1 + b1 + c1
     print("The vetex is ({},{})".format(los, y))
+
+
 get_Value()
 lineOfSym()
 vertex()
 try:
-	quad()
+    quad()
 except ValueError:
     print("There is not x intercept")
