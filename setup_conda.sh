32c306a9d381f7ffacec2ac6cecb82e597a0e2de
Sean Hellum <seanhellum45@gmail.com>
2019-08-07 03:08:51 +0000

    update conda

--- setup_conda.sh
+++ setup_conda.sh
@@ -6,6 +6,14 @@ apt update --yes
 apt upgrade --yes
 
 # Get Miniconda and make it the main Python interpreter
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
-bash ~/miniconda.sh -b -p ~/miniconda
+wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-4.5.11-Linux-x86_64.sh -O ~/miniconda.sh
+/bin/bash ~/miniconda.sh -b -p /opt/conda
 rm ~/miniconda.sh
+/opt/conda/bin/conda clean -tipsy
+ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh
+echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc && \
+echo "conda activate base" >> ~/.bashrc
+env USER='gitpod'
+ENV TINI_VERSION v0.16.1
+ADD https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini /usr/bin/tini
+RUN chmod +x /usr/bin/tini
