36aefb1d95d8eed433696fcf0eb78ebd32fc6020
Sean Hellum <seanhellum45@gmail.com>
2019-08-07 03:17:19 +0000

    update setup_conda

--- setup_conda.sh
+++ setup_conda.sh
@@ -1,19 +1,6 @@
 #! /bin/bash
-
-
-# Setup Ubuntu
-apt update --yes
-apt upgrade --yes
-
-# Get Miniconda and make it the main Python interpreter
-wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-4.5.11-Linux-x86_64.sh -O ~/miniconda.sh
-/bin/bash ~/miniconda.sh -b -p /opt/conda
-rm ~/miniconda.sh
-/opt/conda/bin/conda clean -tipsy
-ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh
-echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc && \
-echo "conda activate base" >> ~/.bashrc
-env USER='gitpod'
-ENV TINI_VERSION v0.16.1
-ADD https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini /usr/bin/tini
-RUN chmod +x /usr/bin/tini
+RUN curl -LO http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh
+RUN bash Miniconda-latest-Linux-x86_64.sh -p /miniconda -b
+RUN rm Miniconda-latest-Linux-x86_64.sh
+ENV PATH=/miniconda/bin:${PATH}
+RUN conda update -y conda
