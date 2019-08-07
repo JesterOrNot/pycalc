d7f3e735ab579dfdd18031572b721dc0ff1da5e4
Sean Hellum <seanhellum45@gmail.com>
2019-08-07 03:38:20 +0000

    test

--- setup_conda.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-RUN curl -LO http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh
-RUN bash Miniconda-latest-Linux-x86_64.sh -p /miniconda -b
-RUN rm Miniconda-latest-Linux-x86_64.sh
-ENV PATH=/miniconda/bin:${PATH}
-RUN conda update -y conda
--- setup_pycalc.sh
+++ setup_pycalc.sh
@@ -8,7 +8,6 @@ pip3 install -r requirements.txt
 python3 setup.py install
 echo 'installed python packages'
 echo 'installing additional dependencies....'
-bash setup_conda.sh
 bash setup_sdkman.sh
 bash setup_heroku.sh
 echo 'setup completed'
