b403aabce6340f880d80bd6fad4d416f97d1f27f
Sean Hellum <seanhellum45@gmail.com>
2019-09-07 03:06:52 +0000

    update

--- setup_pycalc.sh
+++ setup_pycalc.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-echo 'installing dependencies....'
-bash setup_depend.sh
-echo 'dependencies installed.'
 echo 'installing python packages....'
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 python3 setup.py install
 echo 'installed python packages'
-echo 'installing additional dependencies....'
-bash setup_sdkman.sh
-bash setup_heroku.sh
-echo 'setup completed'
