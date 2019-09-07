d7f32842f4b84a6cd6b7fad481e450480eff54dd
Sean Hellum <seanhellum45@gmail.com>
2019-09-07 04:06:25 +0000

    update

---
+++ phys/accelerate.js
@@ -0,0 +1,6 @@
+function acceleration() {
+    readline = require("readline-sync")
+    var initial_velocity = readline.question("What is the initial velocity?: ")
+    var final_velocity = readline.question("What is the final velocity?: ")
+    var startiing time
+}
