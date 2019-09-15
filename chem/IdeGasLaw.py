44e0e9bb26781020e0dae6981fef836049988ed8
Sean Hellum <seanhellum45@gmail.com>
2019-09-15 02:07:55 +0000

    redo some stuff on ideal gas law

--- chem/IdeGasLaw.py
+++ chem/IdeGasLaw.py
@@ -1,41 +1,46 @@
 #! /usr/bin/python3.7
 def igl():
-  unknown = input("What is the unknown variable? (P,V,n,T): ").lower()
-  R = 0.0821
-  if unknown in 'p':
-    V = float(input("What is the volume (in liters) of the gas?: "))
-    n = float(input("How many moles of the gas are present?: "))
-    T = float(input("What is the temperature of the gas (in K?): "))
-    P = (n*R*T)/V
-    print("P = {} atm.".format(P))
-  if unknown in 'v':
-    P = float(input("What is the pressure (in atm) of the gas?: "))
-    n = float(input("How many moles of the gas are present?: "))
-    T = float(input("What is the temperature of the gas (in K?): "))
-    V = (n*R*T)/P
-    print("V = {} liters.".format(V))
-  if unknown in 'n':
-    P = float(input("What is the pressure (in atm) of the gas?: "))
-    V = float(input("What is the volume (in liters) of the gas?: "))
-    T = float(input("What is the temperature of the gas (in K?): "))
-    n = (P*V)/(R*T)
-    print("n = {} moles.".format(n))
-  if unknown in 't':
-    P = float(input("What is the pressure (in atm) of the gas?: "))
-    V = float(input("What is the volume (in liters) of the gas?: "))
-    n = float(input("How many moles of the gas are present?: "))
-    T = (P*V)/(R*n)
-    print("T = {} degrees K.".format(T))
+    unknown = input("What is the unknown variable? (P,V,n,T): ").lower()
+    R = 0.0821
+    if unknown in 'p':
+        V = float(input("What is the volume (in liters) of the gas?: "))
+        n = float(input("How many moles of the gas are present?: "))
+        T = float(input("What is the temperature of the gas (in K?): "))
+        P = (n*R*T)/V
+        print(f"P = {P} atm.")
+    if unknown in 'v':
+        P = float(input("What is the pressure (in atm) of the gas?: "))
+        n = float(input("How many moles of the gas are present?: "))
+        T = float(input("What is the temperature of the gas (in K?): "))
+        V = (n*R*T)/P
+        print(f"V = {V} liters.")
+    if unknown in 'n':
+        P = float(input("What is the pressure (in atm) of the gas?: "))
+        V = float(input("What is the volume (in liters) of the gas?: "))
+        T = float(input("What is the temperature of the gas (in K?): "))
+        n = (P*V)/(R*T)
+        print(f"n = {n} moles.")
+    if unknown in 't':
+        P = float(input("What is the pressure (in atm) of the gas?: "))
+        V = float(input("What is the volume (in liters) of the gas?: "))
+        n = float(input("How many moles of the gas are present?: "))
+        T = (P*V)/(R*n)
+        print(f"T = {T} degrees K.")
+
+
 def convert():
-    convertorno = input('Do we need to convert?: ')
-    if convertorno in 'yes, Yes, yEs, yeS, yES, YES, YEs, YEs, y':
+    convert_or_no = input('Do we need to convert?: ').lower()
+    if convert_or_no in "yes,ye,y":
         uc()
         convert()
-    elif convertorno in 'no, No, nO, NO,n':
+    elif convert_or_no in 'no,n':
         igl()
+
+
 def uc():
-    unknown = input('What Are we converting to Tempature or pressure: ')
-    if unknown in 'temp, Tempature, tempature, t':
+    unknown = input(
+        'What Are we converting to Temperature or pressure: ').lower()
+    if unknown in 't,te,tem,temp':
         def TC():
             c = 0
             decimal = c
@@ -44,69 +49,81 @@ def uc():
             k = 0
             decimal3 = k
             unknown1 = input(
-                'What is the starting temp in, C, K or F: ')
+                'What is the starting temp in, C, K or F: ').lower()
             unknown2 = input(
-                'What are we converting to, C, K or F: ')
-            if unknown1 in 'C, c' and \
-               unknown2 in 'F, f':
+                'What are we converting to, C, K or F: ').lower()
+            if unknown1 == 'c' and \
+               unknown2 == 'f':
                 if decimal2 is str:
-                    c = int(input('What is the starting temp in c that you are converting?: '))
+                    c = int(
+                        input('What is the starting temp in c that you are converting?: '))
                     f = c * 1.8000 + 32
-                    print('{} °F'.format(f))
+                    print(f'{f} °F')
                 else:
-                    c = float(input('What is the starting temp in c that you are converting?: '))
+                    c = float(
+                        input('What is the starting temp in c that you are converting?: '))
                     f = c * 1.8000 + 32
-                    print('{} °F'.format(f))
+                    print(f'{f} °F')
             if unknown1 in 'C, c' and \
                unknown2 in 'K, k':
                 if decimal3 is str:
-                    c = int(input('What is the starting temp in c that you are converting?: '))
+                    c = int(
+                        input('What is the starting temp in c that you are converting?: '))
                     k = c + 273
                     print('{} °K'.format(k))
                 else:
-                    c = float(input('What is the starting temp in c that you are converting?: '))
+                    c = float(
+                        input('What is the starting temp in c that you are converting?: '))
                     k = c + 273
                     print('{} °K'.format(k))
             if unknown1 in 'K, k' and \
                unknown2 in 'F, f':
                 if decimal2 is str:
-                    k = int(input('What is the starting temp in k that you are converting?: '))
+                    k = int(
+                        input('What is the starting temp in k that you are converting?: '))
                     f = k * 1.8 - 459.67
                     print('{} °F'.format(f))
                 else:
-                    k = float(input('What is the starting temp in k that you are converting?: '))
+                    k = float(
+                        input('What is the starting temp in k that you are converting?: '))
                     f = k * 1.8 - 459.67
                     print('{} °F'.format(f))
             if unknown1 in 'K, k' and \
                unknown2 in 'C, c':
                 if decimal is str:
-                    k = int(input('What is the starting temp in k that you are converting?: '))
+                    k = int(
+                        input('What is the starting temp in k that you are converting?: '))
                     c = k - 273
                     print('{} °C'.format(c))
                 else:
-                    k = float(input('What is the starting temp in k that you are converting?: '))
+                    k = float(
+                        input('What is the starting temp in k that you are converting?: '))
                     c = k - 273
                     print('{} °C'.format(c))
             if unknown1 in 'F, f' and \
                unknown2 in 'K, k':
                 if decimal3 is str:
-                    f = int(input('What is the starting temp in k that you are converting?: '))
+                    f = int(
+                        input('What is the starting temp in k that you are converting?: '))
                     k = ((f - 32)/1.8)+273/15
                     print('{} °K'.format(k))
                 else:
-                    f = float(input('What is the starting temp in k that you are converting?: '))
+                    f = float(
+                        input('What is the starting temp in k that you are converting?: '))
                     k = ((f - 32)/1.8)+273/15
                     print('{} °K'.format(k))
             if unknown1 in 'F, f' and \
                unknown2 in 'C, c':
                 if decimal is str:
-                    f = int(input('What is the starting temp in f that you are converting?: '))
+                    f = int(
+                        input('What is the starting temp in f that you are converting?: '))
                     c = (f - 32)/1.8
-                    print('{} °C'.format(c))
+                    print(f'{c} °C')
                 else:
-                    f = float(input('What is the starting temp in f that you are converting?: '))
+                    f = float(
+                        input('What is the starting temp in f that you are converting?: '))
                     c = (f - 32)/1.8
-                    print('{} °C'.format(c))
+                    print(f'{c} °C')
         TC()
     if unknown in 'pressure, p, P':
         def PC():
@@ -123,7 +140,7 @@ def uc():
             unknown2 = input(
                 'What are we converting to? (Kpa, Atm, Torr,Mmhg): ')
             if unknown1 in 'KPA, KPa, Kpa, kPA, kPa, kpA, kpa' and \
-              unknown2 in 'ATM, ATm, Atm, aTM, aTm, atM, atm':
+                    unknown2 in 'ATM, ATm, Atm, aTM, aTm, atM, atm':
                 if decimal is str:
                     kpa = int(input('How much kpa are you converting?: '))
                     atm = kpa / 101.325
@@ -133,7 +150,7 @@ def uc():
                     atm = kpa / 101.325
                     print('{} atm'.format(atm))
             if unknown1 in 'KPA, KPa, Kpa,kPA, kPa, kpA, kpa ' and \
-            unknown2 in 'TORR, TORr, TOrr, Torr, tORR, tORr, tOrr, torr, torR':
+                    unknown2 in 'TORR, TORr, TOrr, Torr, tORR, tORr, tOrr, torr, torR':
                 if decimal2 is str:
                     kpa = int(input('How much kpa are you converting?: '))
                     torr = kpa / 101.3
@@ -143,7 +160,7 @@ def uc():
                     torr = kpa / 0.1333223684
                     print('{} torr'.format(torr))
             if unknown1 in 'KPA, KPa, Kpa,kPA, kPa, kpA, kpa ' and \
-            unknown2 in 'MMHG, MMHg, MMhg, Mmhg, mMHG, mmHg, mMhg, mmhg, mmhG':
+                    unknown2 in 'MMHG, MMHg, MMhg, Mmhg, mMHG, mmHg, mMhg, mmhg, mmhG':
                 if decimal3 is str:
                     kpa = int(input('How much kpa are you converting?: '))
                     mmhg = kpa / 0.1333223684
@@ -153,7 +170,7 @@ def uc():
                     mmhg = kpa / 0.1333223684
                     print('{} mmhg'.format(mmhg))
             if unknown1 in 'MMHG, MMHg, MMhg, Mmhg, mMHG, mmHg, mMhg, mmhg, mmhG' and \
-            unknown2 in 'KPA, KPa, Kpa,kPA, kPa, kpA, kpa' :
+                    unknown2 in 'KPA, KPa, Kpa,kPA, kPa, kpA, kpa':
                 if decimal4 is str:
                     mmhg = int(input('How much mmhg are you converting?: '))
                     kpa2 = mmhg * 0.133322387415
@@ -163,7 +180,7 @@ def uc():
                     kpa2 = mmhg * 0.133322387415
                     print('{} kpa'.format(kpa2))
             if unknown1 in 'MMHG, MMHg, MMhg, Mmhg, mMHG, mmHg, mMhg, mmhg, mmhG' and \
-            unknown2 in 'TORR, TORr, TOrr, Torr, tORR, tORr, tOrr, torr, torR' :
+                    unknown2 in 'TORR, TORr, TOrr, Torr, tORR, tORr, tOrr, torr, torR':
                 if decimal2 is str:
                     mmhg = int(input('How much mmhg are you converting?: '))
                     torr = mmhg * 1
@@ -173,7 +190,7 @@ def uc():
                     torr = mmhg * 1
                     print('{} torr'.format(torr))
             if unknown1 in 'MMHG, MMHg, MMhg, Mmhg, mMHG, mmHg, mMhg, mmhg, mmhG' and \
-            unknown2 in 'ATM, ATm, Atm, aTM, aTm, atM, atm' :
+                    unknown2 in 'ATM, ATm, Atm, aTM, aTm, atM, atm':
                 if decimal is str:
                     mmhg = int(input('How much mmhg are you converting?: '))
                     atm = mmhg / 760
@@ -183,7 +200,7 @@ def uc():
                     atm = mmhg / 760
                     print('{} atm'.format(atm))
             if unknown1 in 'ATM, ATm, Atm, aTM, aTm, atM, atm' and \
-            unknown2 in 'MMHG, MMHg, MMhg, Mmhg, mMHG, mmHg, mMhg, mmhg, mmhG' :
+                    unknown2 in 'MMHG, MMHg, MMhg, Mmhg, mMHG, mmHg, mMhg, mmhg, mmhG':
                 if decimal3 is str:
                     atm = int(input('How much atm are you converting?: '))
                     mmhg = atm * 760
@@ -193,7 +210,7 @@ def uc():
                     mmhg = atm * 760
                     print('{} mmhg'.format(mmhg))
             if unknown1 in 'ATM, ATm, Atm, aTM, aTm, atM, atm' and \
-            unknown2 in 'TORR, TORr, TOrr, Torr, tORR, tORr, tOrr, torr, torR':
+                    unknown2 in 'TORR, TORr, TOrr, Torr, tORR, tORr, tOrr, torr, torR':
                 if decimal2 is str:
                     atm = int(input('How much atm are you converting?: '))
                     torr = atm * 760
@@ -203,7 +220,7 @@ def uc():
                     torr = atm * 760
                     print('{} torr'.format(torr))
             if unknown1 in 'ATM, ATm, Atm, aTM, aTm, atM, atm' and \
-            unknown2 in 'KPA, KPa, Kpa,kPA, kPa, kpA, kpa':
+                    unknown2 in 'KPA, KPa, Kpa,kPA, kPa, kpA, kpa':
                 if decimal4 is str:
                     atm = int(input('How much atm are you converting?: '))
                     kpa2 = atm * 101.325
@@ -213,7 +230,7 @@ def uc():
                     kpa2 = atm * 101.325
                     print('{} kpa'.format(kpa2))
             if unknown1 in 'TORR, TORr, TOrr, Torr, tORR, tORr, tOrr, torr, torR' and \
-            unknown2 in 'KPA, KPa, Kpa,kPA, kPa, kpA, kpa':
+                    unknown2 in 'KPA, KPa, Kpa,kPA, kPa, kpA, kpa':
                 if decimal4 is str:
                     torr = int(input('How much torr are you converting?: '))
                     kpa2 = torr * 0.1333223684
@@ -223,7 +240,7 @@ def uc():
                     kpa2 = torr * 0.1333223684
                     print('{} kpa'.format(kpa2))
             if unknown1 in 'TORR, TORr, TOrr, Torr, tORR, tORr, tOrr, torr, torR' and \
-            unknown2 in 'MMHG, MMHg, MMhg, Mmhg, mMHG, mmHg, mMhg, mmhg, mmhG':
+                    unknown2 in 'MMHG, MMHg, MMhg, Mmhg, mMHG, mmHg, mMhg, mmhg, mmhG':
                 if decimal3 is str:
                     torr = int(input('How much torr are you converting?: '))
                     mmhg = torr / 1
@@ -233,14 +250,16 @@ def uc():
                     mmhg = torr / 1
                     print('{} mmhg'.format(mmhg))
             if unknown1 in 'TORR, TORr, TOrr, Torr, tORR, tORr, tOrr, torr, torR' and \
-            unknown2 in 'ATM, ATm, Atm, aTM, aTm, atM, atm':
+                    unknown2 in 'ATM, ATm, Atm, aTM, aTm, atM, atm':
                 if decimal is str:
                     torr = int(input('How much torr are you converting?: '))
                     atm = torr / 760
-                    print('{} atm'.format(atm))
+                    print(f'{atm} atm')
                 else:
                     torr = float(input('How much torr are you converting: '))
                     atm = torr / 760
-                    print('{} atm'.format(atm))
+                    print(f'{atm} atm')
         PC()
+
+
 convert()
