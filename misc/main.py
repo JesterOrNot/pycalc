73f17489c65099818b8e9d953078c3d8f67f8e49
Blurryface05 <seahel1@nilesk12.org>
2019-09-23 01:45:45 +0000

    styles

--- misc/main.py
+++ misc/main.py
@@ -1,9 +1,12 @@
-import os
 print("Welcome to misc calcs this is just a bunch of handy little calcs that couldn't be placed in other catagories but I still thought might be useful to someone enjoy the available calculators are: Gpa calc(gpa)")
+
+
 def choose():
     usrInt = input("What Calc do you want?: ").lower()
     if usrInt in 'gpa':
         import misc.gpa
     else:
         print("An error has occurred\nPlease try again")
+
+
 choose()
